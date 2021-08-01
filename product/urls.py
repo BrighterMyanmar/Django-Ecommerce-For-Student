4aff8584458f4503920e8360c03a9b8e1e217cd1
Brighter Myanmar <brightermyanmar@gmail.com>
2021-08-01 19:12:15 +0630

    More Data :

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('',views.products,name="all-products")
+    path('',views.products,name="all-products"),
+    path('cartview/<str:ost>',views.cartview,name="cart-view"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product 
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 def products(request):
     products = Product.objects.all();
@@ -7,3 +9,9 @@ def products(request):
         'products':Product.objects.all()
     }
     return render(request,'product/index.html',context)
+
+def cartview(request,ost): # "1,2,3,4,5"
+    productList = ost.split(',') # [1,2,3,4,5]
+    products = Product.objects.filter(pk__in=productList).values('id','name','price','image')
+    product = json.dumps(list(products),cls=DjangoJSONEncoder)
+    return render(request,'product/cartview.html',{'product':product})
