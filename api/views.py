bacb1e3fcc1ea49388207605bea58d5a16a12a8a
Brighter Myanmar <brightermyanmar@gmail.com>
2021-08-15 18:47:14 +0630

    Django Finish

--- api/views.py
+++ api/views.py
@@ -3,10 +3,13 @@ from rest_framework import viewsets
 from category.models import Category
 from product.models import Product
 from .serializer import CategorySerializer,ProductSerializer
+from rest_framework import permissions
 
 class CategoryView(viewsets.ModelViewSet) :
     queryset = Category.objects.all() 
     serializer_class = CategorySerializer
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly] # get post ptach delete
+    #http_method_names = ['get','post','patch','delete']
 
 class ProductView(viewsets.ModelViewSet) :
     queryset = Product.objects.all() 
--- aws/urls.py
+++ aws/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('user/',include('user.urls')),
     path('',include('product.urls')),
     path('api/',include('api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG :
